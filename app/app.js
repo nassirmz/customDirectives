e18e2d4c9eabe1b945daf164b7cce2350d7522c3
nassir <nassirmz@gmail.com>
2017-02-22 12:07:58 -0800

    Change to ui-router

--- app/app.js
+++ app/app.js
@@ -1,15 +1,19 @@
 angular
-  .module('app', ['app.users', 'ngRoute'])
+  .module('app', ['ui.router', 'app.oneUser', 'app.users'])
   .config(config);
 
-  function config ($routeProvider) {
-    $routeProvider
-      .when('/oneUser', {
+  function config ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/users');
+
+    $stateProvider
+      .state('oneUser', {
+        url: '/oneUser',
         templateUrl: 'components/oneUser/oneUser.html',
         controller: 'OneUserController',
         controllerAs: 'oneUserCtrl'
       })
-      .when('/', {
+      .state('users', {
+        url: '/users',
         templateUrl: 'components/users/users.html',
         controller: 'UsersController',
         controllerAs: 'usersCtrl',
